8567f49968137507e5d6d9bfc938f8e2f836ad4a
Tim Ludikar <tim.ludikar@rangle.io>
2018-06-20 11:32:18 -0400

    Adjusting slide in CSS effect

--- mibank-components/www/build/mibank/mi-navigation.js
+++ mibank-components/www/build/mibank/mi-navigation.js
@@ -51,7 +51,7 @@ class MiNavigation {
             "method": true
         }
     }; }
-    static get style() { return ".sidenav {\n  height: 100%;\n  width: 0;\n  position: fixed;\n  z-index: 1;\n  top: 0;\n  left: 0;\n  background-color: #111;\n  overflow-x: hidden;\n  padding-top: 60px;\n  -webkit-transition: 0.5s;\n  transition: 0.5s;\n}\n\n.sidenav.open {\n  width: 250px;\n}\n\n.sidenav a {\n  padding: 8px 8px 8px 32px;\n  text-decoration: none;\n  font-size: 25px;\n  color: #818181;\n  display: block;\n  -webkit-transition: 0.3s;\n  transition: 0.3s;\n}\n\n.sidenav a:hover {\n  color: #f1f1f1;\n}\n\n.sidenav .closebtn {\n  position: absolute;\n  top: 0;\n  right: 25px;\n  font-size: 36px;\n  margin-left: 50px;\n}"; }
+    static get style() { return ".sidenav {\n  height: 100%;\n  width: 250px;\n  position: fixed;\n  z-index: 1;\n  top: 0;\n  left: -250px;\n  background-color: #111;\n  overflow-x: hidden;\n  padding-top: 60px;\n  -webkit-transition: 0.5s;\n  transition: 0.5s;\n}\n\n.sidenav.open {\n  left: 0;\n}\n\n.sidenav a {\n  padding: 8px 8px 8px 32px;\n  text-decoration: none;\n  font-size: 25px;\n  color: #818181;\n  display: block;\n  -webkit-transition: 0.3s;\n  transition: 0.3s;\n}\n\n.sidenav a:hover {\n  color: #f1f1f1;\n}\n\n.sidenav .closebtn {\n  position: absolute;\n  top: 0;\n  right: 25px;\n  font-size: 36px;\n  margin-left: 50px;\n}"; }
 }
 
 export { MiNavigation };
