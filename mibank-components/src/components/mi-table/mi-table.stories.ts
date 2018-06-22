80fc563798bf535b15e6e99c7480ddb57bc9c243
Tim Ludikar <tim.ludikar@rangle.io>
2018-06-22 15:56:51 -0400

    Updating storybook

--- mibank-components/src/components/mi-table/mi-table.stories.ts
+++ mibank-components/src/components/mi-table/mi-table.stories.ts
@@ -1,14 +1,15 @@
 import { storiesOf } from "@storybook/html";
+import { withKnobs, array } from "@storybook/addon-knobs";
 import { HTMLTableElement } from "./mi-table";
 
-const stories = storiesOf("Table", module);
+const stories = storiesOf("Table", module).addDecorator(withKnobs);
 stories.add("default", () => {
-  const headings = ["Account Name", "Amount"];
-  const data = [
+  const headings = array("Heading", ["Account Name", "Amount"]);
+  const data = array("Data", [
     ["Account 1", "$1000"],
     ["Account 2", "$2000"],
     ["Account 3", "$3000"]
-  ];
+  ]);
 
   const table: HTMLTableElement = <HTMLTableElement>(
     document.createElement("mi-table")
